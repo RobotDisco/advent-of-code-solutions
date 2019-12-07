f1e26eedbdca5215ceb03c873acb070c7550508c
Gaelan D'costa <gdcosta@gmail.com>
2019-12-07 16:03:53 -0500

    The most basic of Haskell test harnesses.

--- AOC2019/test/Spec.hs
+++ AOC2019/test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+test1 :: TestTree
+test1 = testCase "Equals 1" $ 1 @?= 1
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain test1
