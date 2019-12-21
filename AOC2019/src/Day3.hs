44434ef3a62cb9e0cdca13410f5ce0294f0da0dc
Gaelan D'costa <gdcosta@gmail.com>
2019-12-21 17:51:03 -0500

    Adding explicit types for 2019 Day3 Star 1

--- AOC2019/src/Day3.hs
+++ AOC2019/src/Day3.hs
@@ -8,16 +8,22 @@ import Data.List.Split (splitOn)
 import qualified Data.Set as S
 import qualified Data.Text as T
 
--- type Point2D = (Int, Int)
+type Point2D = (Int, Int)
 
 
+up :: (Point2D -> Point2D, Point2D -> Int -> Point2D)
 up = (\(x,y) -> (x, y+1), \(x,y) l -> (x, y+l))
+down :: (Point2D -> Point2D, Point2D -> Int -> Point2D)
 down = (\(x,y) -> (x, y-1), \(x,y) l -> (x, y-l))
+left :: (Point2D -> Point2D, Point2D -> Int -> Point2D)
 left = (\(x,y) -> (x-1, y), \(x,y) l -> (x-l, y))
+right :: (Point2D -> Point2D, Point2D -> Int -> Point2D)
 right = (\(x,y) -> (x+1, y), \(x,y) l -> (x+l, y))
 
+manhattanDistance :: (Num a) => (a, a) -> a
 manhattanDistance (x, y) = (abs x) + (abs y)
 
+addSegment :: (Point2D, S.Set Point2D) -> String -> (Point2D, S.Set Point2D)
 addSegment (currEnd, coordsInWire) (dir:strLength) =
   let segLength = read strLength
       fs = case dir of
@@ -28,6 +34,7 @@ addSegment (currEnd, coordsInWire) (dir:strLength) =
         _ -> error "This should never happen!"
   in (((snd fs) currEnd segLength), S.union coordsInWire $ S.fromList $ take segLength $ tail $ iterate (fst fs) currEnd)
         
+connectionDistance :: String -> Int
 connectionDistance input =
   let initEnd = (0, 0)
       initCoords = S.empty
@@ -42,4 +49,5 @@ connectionDistance input =
   in
     head . sort $ map manhattanDistance intersections
 
+star1 :: T.Text -> T.Text
 star1 input = (T.pack . show . connectionDistance . T.unpack) input
